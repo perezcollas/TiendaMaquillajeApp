5574c7e2d27e5fafcf07606172fe8c9144fea86a
perezcollas <hperez5@ucol.mx>
2025-05-23 14:01:31 -0600

    Error en ProductoService.cs

---
+++ TiendaMaquillajeApp/Data/IClienteService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TiendaMaquillajeApp.Data;
+
+namespace TiendaMaquillajeApp.Data
+{
+    public interface IClienteService
+    {
+        Task<List<Cliente>> ObtenerClientesAsync();
+        Task AgregarClienteAsync(Cliente cliente);
+        Task EditarClienteAsync(Cliente cliente);
+        Task EliminarClienteAsync(int idCliente);
+    }
+}
